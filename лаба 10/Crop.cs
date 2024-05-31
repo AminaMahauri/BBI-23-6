0aa86725f14b2b36d530f203b107a0c306085c5e
AminaMahauri <146061239+AminaMahauri@users.noreply.github.com>
2024-05-31 17:03:32 +0300

    код в папке

---
+++ лаба 10/Crop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab_Lerok
+{
+    public struct Crop
+    {
+        public string CropName { set; get; }
+        public int Season { set; get; }
+        public string Purpose { set; get; }
+
+        // Конструктор
+        [JsonConstructor]
+        public Crop() { }
+        public Crop(string name, int season, string purpose)
+        {
+            this.CropName = name;
+            this.Season = season;
+            this.Purpose = purpose;
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {CropName}, выращен в {Season}, назначение: {Purpose}";
+        }
+    }
+}
---
+++ лаба 10/IHarvestCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Lerok
+{
+    interface IHarvestCalculator
+    {
+        public double GetYield();
+        public double GetYieldPerDay();
+        public double GetAvgYield();
+        public double GetAllYield();
+    }
+}
